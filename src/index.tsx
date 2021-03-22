8455027edb9db16898c45dc6d6c8f696d80feb94
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-22 00:11:28 +0000

    started table component implementation ... diving in now!

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@
 
 export { default } from './v1/use-table'
 export { default as usePagination } from './v1/use-pagination'
+export { default as Table } from './v1/table'
---
+++ src/v1/table/table.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const TableComponent = () => {
+  return <React.Fragment></React.Fragment>
+}
+
+export default TableComponent
