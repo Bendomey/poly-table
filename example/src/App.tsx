f07a0cf361d3ee8ffb27df6677cc151b48645f68
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-22 00:10:12 +0000

    added table component example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import 'poly-table/dist/index.css'
 import WithHookImplmentation from './with-hook'
+import WithTableImplmentation from './with-table-impl'
 
 const App = () => {
   return (
     <div>
       <span>Implementation with hook</span>
       <WithHookImplmentation />
+      <span>Implementation with table component</span>
+      <WithTableImplmentation />
     </div>
   )
 }
