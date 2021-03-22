f72ec7d0de6ff41f65777f6b13910bcaf3c90586
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-22 03:09:54 +0000

    added some functions to get some data in table cell component

--- src/v1/broker/broker.tsx
+++ src/v1/broker/broker.tsx
@@ -7,3 +7,28 @@ export const getHeaderDetails = (cols: ColumnProps[]): HeaderDetailsProps[] =>
       headerStyle: col?.headerStyle
     }
   })
+
+export const getAccessor = (accessor: any, data: any, optional = null) =>
+  accessor
+    .split('.')
+    .reduce((o: any, i: number) => o[i] || optional || 'N/A', data)
+
+export const getText = (arr: any[], data: any) => {
+  let newString: string = ''
+  arr.map((word) => {
+    newString += `${
+      getAccessor(word, data)?.replace(/(<([^>]+)>)/gi, '') || 'Not Specified'
+    } `
+  })
+  return newString
+}
+
+export const truncate = (data: string, truncate: number | null = null) => {
+  if (truncate) {
+    if (data.split('').length > truncate) {
+      return data.split('').slice(0, truncate).join('') + '...'
+    }
+    return data
+  }
+  return data
+}
