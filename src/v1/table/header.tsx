7e9d02bbb7f9df34242c137ce181f2a4c4a9682e
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-22 03:10:20 +0000

    header table implementation

--- src/v1/table/header.tsx
+++ src/v1/table/header.tsx
@@ -4,12 +4,12 @@ import { HeaderDetailsProps, HeaderProps } from '../types'
 const Header: React.FC<HeaderProps> = ({ col: cols }) => {
   return (
     <React.Fragment>
-      <tr className={'flex flex-row'}>
+      <tr className={'flex flex-row border-b border-gray-200'}>
         {cols.map((col: HeaderDetailsProps, i: number) => (
           <React.Fragment key={i}>
             <th
               style={{ flex: col?.headerStyle?.flex || 1 }}
-              className={`border-gray-100 px-6 py-3 border-b border-gray-200 bg-gray-50 text-${
+              className={` px-6 py-3 border-none  bg-gray-50 text-${
                 col?.headerStyle?.align || 'left'
               } text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider`}
             >
