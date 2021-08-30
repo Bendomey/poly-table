5319d1c8857c2925e061de2a3e1c950cc0dd56b5
Benjamin Domey <domeybenjamin1@gmail.com>
2021-08-30 12:48:51 +0000

    Updates

---
+++ precache-manifest.e71f87df87fa28031e294ed136780521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38a95b28f91c271e4d0c05469055c64",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac5fb3618d72cef656e7",
+    "url": "./static/css/main.80038f4e.chunk.css"
+  },
+  {
+    "revision": "49d166220c5ed57db837",
+    "url": "./static/js/2.63c89ef4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.63c89ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5fb3618d72cef656e7",
+    "url": "./static/js/main.d2517f85.chunk.js"
+  },
+  {
+    "revision": "7b143b7105c07c1af3c3",
+    "url": "./static/js/runtime-main.db108a99.js"
+  }
+]);
